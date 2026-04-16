1157f5ef2b8b4e5327158d20aee506e3c3584501
mark <78883486+MarkBrooks3@users.noreply.github.com>
2026-04-15 20:56:43 -0500

    added ui to the game

--- index.js
+++ index.js
@@ -1,63 +1,65 @@
-function playGame() {
-  let playerScore = 0;
-  let computerScore = 0;
+let playerScore = 0;
+let computerScore = 0;
+let resultsScreen = document.querySelector('#results');
+let playerScoreboard = document.querySelector('#playerScore');
+let computerScoreboard = document.querySelector('#computerScore');
 
-  function getComputerChoice() {
-    let x = Math.random();
-    if (x < 0.3333333333) {
+function getComputerChoice() {
+  let x = Math.floor(Math.random() * 3);
+  switch (x) {
+    case 0:
       return 'rock';
-    } else if (x < 0.6666666666) {
+    case 1:
       return 'paper';
-    } else {
+    case 2:
       return 'scissors';
-    }
   }
-
-  function getHumanChoice() {
-    return prompt('Rock, Paper, Scissors shoot...');
-  }
-
-  function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    if (computerChoice == 'rock') {
-      if (humanChoice == 'rock') {
-        console.log("It's a tie no points");
-      } else if (humanChoice == 'paper') {
-        playerScore++;
-        console.log('Paper beats rock , you win!');
-      } else {
-        computerScore++;
-        console.log('Rock beats scissors, you lose');
-      }
-    } else if (computerChoice == 'paper') {
-      if (humanChoice == 'rock') {
-        computerScore++;
-        console.log('Paper beats rock, you lose');
-      } else if (humanChoice == 'paper') {
-        console.log("It's a tie no points");
-      } else {
-        playerScore++;
-        console.log('Scissors beats paper, you win!');
-      }
-    } else {
-      if (humanChoice == 'rock') {
-        playerScore++;
-        console.log('Rock beats scissors, you win!');
-      } else if (humanChoice == 'paper') {
-        computerScore++;
-        console.log('Scissors beats paper, you lose');
-      } else {
-        console.log("It's a tie no points");
-      }
-    }
-  }
-
-  for (let i = 0; i < 5; i++) {
-    playRound(getHumanChoice(), getComputerChoice());
-  }
-
-  console.log(`player score:${playerScore}`);
-  console.log(`computer score:${computerScore}`);
 }
 
-playGame();
+function checkWinner() {
+  if (playerScore == 5) {
+    alert('You won :)');
+    playerScore = 0;
+    computerScore = 0;
+    computerScoreboard.innerText = `Computer Score: ${computerScore}`;
+    playerScoreboard.innerText = `Player Score: ${playerScore}`;
+  } else if (computerScore == 5) {
+    alert('You lost :(');
+    playerScore = 0;
+    computerScore = 0;
+    computerScoreboard.innerText = `Computer Score: ${computerScore}`;
+    playerScoreboard.innerText = `Player Score: ${playerScore}`;
+  }
+}
+
+function playRound(humanChoice) {
+  let computerChoice = getComputerChoice();
+  humanChoice = humanChoice.toLowerCase();
+  if (
+    (computerChoice == 'rock' && humanChoice == 'rock') ||
+    (computerChoice == 'paper' && humanChoice == 'paper') ||
+    (computerChoice == 'scissors' && humanChoice == 'scissors')
+  ) {
+    resultsScreen.innerText = "It's a tie no points";
+  } else if (
+    (computerChoice == 'rock' && humanChoice == 'scissors') ||
+    (computerChoice == 'paper' && humanChoice == 'rock') ||
+    (computerChoice == 'scissors' && humanChoice == 'paper')
+  ) {
+    resultsScreen.innerText = 'you lose';
+    computerScore++;
+    computerScoreboard.innerText = `Computer Score: ${computerScore}`;
+    checkWinner();
+  } else {
+    resultsScreen.innerText = 'you win';
+    playerScore++;
+    playerScoreboard.innerText = `Player Score: ${playerScore}`;
+    checkWinner();
+  }
+}
+
+let buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) =>
+  button.addEventListener('click', (e) => playRound(e.target.innerText))
+);
